3660d0b51c069daa76814d8d575677be21fe974f
Toby Burress <kurin@delete.org>
2013-08-03 23:58:34 -0400

    A quick and dirty transport.

---
+++ raft/udp.py
@@ -0,0 +1,38 @@
+import socket
+import errno
+import hashlib
+import select
+
+from event import Event
+
+class UDP(object):
+    def __init__(self, port):
+        self.port = port
+        self.handlers = Event()
+
+    def start(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", self.port))
+
+    def recv(self):
+        recv, _, _ = select.select([self.sock], [], [], 0.250)
+        if recv:
+            try:
+                return self.sock.recvfrom(65535)  # max udp size
+            except socket.error as e:
+                pass  # just drop it on the floor
+
+    def send(self, msg, dst):
+        try:
+            self.sock.sendto(msg, dst)
+        except socket.error as e:
+            if e[0] == errno.EPIPE:
+                self.shutdown()
+                self.start()
+
+    def shutdown(self):
+        try:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.sock.close()
+        except:  # nobody cares
+            pass
