4053a020953a8f12546dd3f22226104d4556c51a
Toby Burress <kurin@delete.org>
2013-08-04 00:25:48 -0400

    test

---
+++ tests/unit/test_store.py
@@ -0,0 +1,21 @@
+import pytest
+from mock import Mock, MagicMock
+
+def test_write(monkeypatch):
+    import os
+    import msgpack
+    import raft.store as store
+
+    mock_open = MagicMock()
+    monkeypatch.setattr(store, 'open', mock_open, raising=False)
+    fobj = MagicMock()
+    mock_open().__enter__.return_value = fobj
+
+    vals = (25,
+            'mr excalibur',
+            [(1, 1, 'a msg'), (2, 1, 'another msg')],
+            {'mr excalibur': ('192.168.0.15', 2995)},
+            'conan')
+    packed = msgpack.packb(vals)
+    store.write_state(*vals)
+    fobj.write.assert_called_with(packed)
