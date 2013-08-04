0ff2336c71f189a6951259549b424d9887d284f7
Toby Burress <kurin@delete.org>
2013-08-04 08:00:15 -0400

    iterating to maybe

--- raft/server.py
+++ raft/server.py
@@ -34,17 +34,18 @@ class Server(object):
             print self.role
             ans = self.udp.recv()
             if ans is not None:
-                msg, _ = ans
-                self.handle_message(msg)
+                msg, addr = ans
+                self.handle_message(msg, addr)
             else:
                 self.handle_nomessage()
 
-    def handle_message(self, msg):
+    def handle_message(self, msg, addr):
         msg = msgpack.unpackb(msg)
         mtype = msg['type']
-        term = msg['term']
+        term = msg.get('term', None)
+        msg['src'] = addr
         # no matter what, if our term is old, update and step down
-        if term > self.term:
+        if term and term > self.term:
             self.term = term
             self.voted = None
             self.role = 'follower'
@@ -52,9 +53,38 @@ class Server(object):
         if not hasattr(self, mname):
             return  # nothing to do
         getattr(self, mname)(msg)
+        if self.role == 'leader':
+            # send heartbeats when handling messages as well
+            self.send_ae()
+
+    def handle_msg_leader_ae_reply(self, msg):
+        success = msg['success']
+        uuid = msg['id']
+        if success:
+            self.next_index[uuid] = msg['index']
+        else:
+            self.next_index[uuid] -= 1
 
     def handle_msg_follower_ae(self, msg):
         self.last_update = time.time()
+        self.leader = msg['id']
+        logs = msg['entries']
+        if not logs:
+            # just a heartbeat
+            return
+        addr = self.peers[self.leader]
+        previdx = msg['previdx']
+        prevterm = msg['prevterm']
+        if not self.log.exists(previdx, prevterm):
+            rpc = self.ae_rpc_reply(False)
+            self.udp.send(rpc, addr)
+        for ent in sorted(logs):
+            term = logs[ent][0]
+            msg = logs[ent][2]
+            msgid = msg['id']
+            self.log.add(term, msgid, msg)
+        rpc = self.ae_rpc_reply(True)
+        self.udp.send(rpc, addr)
 
     def handle_msg_candidate_ae(self, msg):
         # someone else was elected during our candidacy
@@ -64,6 +94,17 @@ class Server(object):
             return
         self.role = 'follower'
         self.handle_msg_follower_ae()
+
+    def handle_msg_follower_cq(self, msg):
+        rpc = self.cr_rdr_rpc()
+        src = msg['src']
+        self.udp.send(rpc, src)
+
+    def handle_msg_leader_cq(self, msg):
+        self.log.add(self.term, msg['id'], msg)
+        rpc = self.cr_rpc(msg['id'], 'ok')
+        src = msg['src']
+        self.udp.send(rpc, src)
 
     def handle_msg_candidate_rv(self, msg):
         # don't vote for a different candidate!
@@ -78,7 +119,7 @@ class Server(object):
     def handle_msg_follower_rv(self, msg):
         term = msg['term']
         uuid = msg['id']
-        olog = {msg['log_index']: (msg['log_term'], None, None)}
+        olog = {msg['log_index']: (msg['log_term'], None, {})}
         olog = RaftLog(olog)
         addr = self.peers[uuid]
         if term < self.term:
@@ -133,12 +174,17 @@ class Server(object):
             self.call_election()
         elif self.role == 'leader':
             # send a heartbeat
-            for uuid in self.peers:
-                if uuid == self.uuid:
-                    continue
-                rpc = self.ae_rpc(uuid)
-                addr = self.peers[uuid]
-                self.udp.send(rpc, addr)
+            self.send_ae()
+
+    def send_ae(self):
+        for uuid in self.peers:
+            if uuid == self.uuid:
+                continue
+            logs = self.log.logs_after_index(self.next_index[uuid])
+            rpc = self.ae_rpc(uuid, logs)
+            print "sent %s" % logs
+            addr = self.peers[uuid]
+            self.udp.send(rpc, addr)
 
     def call_election(self):
         self.term += 1
@@ -180,15 +226,44 @@ class Server(object):
         }
         return msgpack.packb(rpc)
 
-    def ae_rpc(self, peeruuid, append=[]):
+    def ae_rpc(self, peeruuid, append={}):
         previdx = self.next_index[peeruuid]
         rpc = {
             'type': 'ae',
             'term': self.term,
             'id': self.uuid,
             'previdx': previdx,
-            'prevterm': self.log.get(previdx)[1],
+            'prevterm': self.log.get(previdx)[0],
             'entries': append,
             'commitidx': self.commitidx,
+        }
+        return msgpack.packb(rpc)
+
+    def ae_rpc_reply(self, success):
+        rpc = {
+            'type': 'ae_reply',
+            'term': self.term,
+            'id': self.uuid,
+            'index': self.log.maxindex(),
+            'success': success
+        }
+        return msgpack.packb(rpc)
+
+    def cr_rpc(self, qid, ans):
+        # client response RPC
+        # qid = query id, ans is arbitrary data
+        rpc = {
+            'type': 'cr',
+            'id': qid,
+            'data': ans
+        }
+        return msgpack.packb(rpc)
+
+    def cr_rdr_rpc(self):
+        # client response redirect; just point them
+        # at the master
+        rpc = {
+            'type': 'cr_rdr',
+            'master': self.peers[self.leader]
         }
         return msgpack.packb(rpc)
