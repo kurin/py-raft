2f621972ba7d430ee20a99e892e3f722c526b314
Toby Burress <kurin@delete.org>
2013-08-18 21:44:31 -0400

    test_dump

--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -2,41 +2,46 @@ import pytest
 
 from raft import log
 
-def logentry(index, term, committed=False, msgid='', msg={}):
+def mle(index, term, committed=False, msgid='', msg={}):
     return dict(index=index, term=term, committed=committed,
                 msgid=msgid, msg=msg)
 
 def test_le():
     # a's term is greater than b's
-    a = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 4)}
-    b = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 3)}
+    a = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 4)}
+    b = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 3)}
     ra = log.RaftLog(a)
     rb = log.RaftLog(b)
     assert ra > rb
     # terms are equal
-    a = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 4)}
-    b = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 4)}
+    a = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 4)}
+    b = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 4)}
     ra = log.RaftLog(a)
     rb = log.RaftLog(b)
     assert a <= b
     assert b <= a
     # terms equal but more commits in b
-    a = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 4)}
-    b = {1: logentry(1, 2),
-         2: logentry(2, 2),
-         3: logentry(3, 4),
-         4: logentry(4, 4)}
+    a = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 4)}
+    b = {1: mle(1, 2),
+         2: mle(2, 2),
+         3: mle(3, 4),
+         4: mle(4, 4)}
     ra = log.RaftLog(a)
     rb = log.RaftLog(b)
     assert rb > ra
 
+def test_dump():
+    rl = log.RaftLog(None)
+    dump = {0: {'term': 0, 'msgid': '', 'committed': True,
+                'acked': [], 'msg': {}, 'index': 0}}
+    assert rl.dump() == dump
