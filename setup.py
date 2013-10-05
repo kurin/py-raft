40141b4f48a2837d5e764b0b0900b108a31398c0
Toby Burress <kurin@delete.org>
2013-10-04 20:30:10 -0400

    pypi stuff

--- setup.py
+++ setup.py
@@ -12,10 +12,18 @@ except ImportError:
 with open('raft/version.txt') as v:
     version = v.read().strip()
 
+classifiers = '''
+Development Status :: 1 - Planning
+License :: Public Domain
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+'''
+
 setup(name='py-raft',
       version=version,
       description='The RAFT Consensus Algorithm',
       author='Toby Burress',
       author_email='kurin@delete.org',
+      classifiers = filter(None, classifiers.split('\n')),
       package_data={'': ['version.txt']},
       packages=['raft'])
